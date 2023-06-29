8d3bd9cd4d1f72889d843e39e0f1189c489426d5
Eric Rocha <ericrochaeric@gmail.com>
2023-06-29 11:31:30 -0300

    :tada: project init

--- App.tsx
+++ App.tsx
@@ -5,6 +5,7 @@ export default function App() {
   return (
     <View className="flex-1 items-center justify-center bg-gray-950">
       <Text className="text-5xl font-bold text-gray-50">Hello World!</Text>
+      <Text className="text-5xl font-bold text-gray-50">Project Start!</Text>
       <StatusBar style="inverted" translucent />
     </View>
   )
